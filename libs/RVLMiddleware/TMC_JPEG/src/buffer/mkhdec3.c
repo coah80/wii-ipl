77316c0ff11ff3e23166675bf92a90d7ca741914
coah <100182500+coah80@users.noreply.github.com>
2026-09-19 22:50:03 +0000

    Match TMCJPEGDEC_make_huffdec

--- libs/RVLMiddleware/TMC_JPEG/src/buffer/mkhdec3.c
+++ libs/RVLMiddleware/TMC_JPEG/src/buffer/mkhdec3.c
@@ -1,5 +1,256 @@
 #include <tmc_jpeg_internal.h>
 
+extern void* memcpy(void* dest, const void* src, unsigned long size);
+extern void* memset(void* dest, int value, unsigned long size);
+
+#ifdef __MWERKS__
+asm s32 TMCJPEGDEC_make_huffdec(register const u8* dht, register const u8* tb, register TMCHuffParam* hp) {
+    nofralloc
+
+    stwu r1, -0x840(r1)
+    mflr r0
+    li r6, 1
+    li r7, 1
+    stw r0, 0x844(r1)
+    stmw r20, 0x810(r1)
+    mr r26, r3
+    mr r27, r4
+    addi r4, r1, 0x408
+    addi r3, r3, 1
+    lwz r31, 0(r5)
+    lwz r30, 4(r5)
+    lwz r29, 8(r5)
+    lbz r28, 0xc(r5)
+    li r5, 0
+
+_make_huffdec_count_loop:
+    lbz r0, 0(r3)
+    cmpw r7, r0
+    ble _make_huffdec_count_store
+    addi r6, r6, 1
+    li r7, 1
+    cmpwi r6, 0x10
+    addi r3, r3, 1
+    ble _make_huffdec_count_loop
+    slwi r0, r5, 2
+    addi r7, r1, 0x408
+    li r3, 0
+    stwx r3, r7, r0
+    b _make_huffdec_count_done
+
+_make_huffdec_count_store:
+    addi r5, r5, 1
+    stw r6, 0(r4)
+    cmpw r5, r28
+    addi r4, r4, 4
+    ble _make_huffdec_count_next
+    li r3, -0x40
+    b _make_huffdec_return
+
+_make_huffdec_count_next:
+    addi r7, r7, 1
+    b _make_huffdec_count_loop
+
+_make_huffdec_count_done:
+    lwz r0, 0x408(r1)
+    addi r6, r1, 8
+    li r9, 0
+    li r8, 0
+    clrlwi r10, r0, 24
+    li r4, 1
+    b _make_huffdec_values_loop
+
+_make_huffdec_values_inner:
+    clrlwi r5, r10, 24
+    slwi r3, r8, 2
+    b _make_huffdec_values_store_check
+
+_make_huffdec_values_store:
+    clrlwi r0, r9, 16
+    addi r7, r7, 4
+    stwx r0, r6, r3
+    addi r8, r8, 1
+    addi r3, r3, 4
+    addi r9, r9, 1
+
+_make_huffdec_values_store_check:
+    lwz r0, 0(r7)
+    clrlwi r0, r0, 24
+    cmplw r5, r0
+    beq _make_huffdec_values_store
+    clrlwi r3, r9, 16
+    slw r0, r4, r5
+    cmpw r3, r0
+    blt _make_huffdec_values_next
+    li r3, -0x40
+    b _make_huffdec_return
+
+_make_huffdec_values_next:
+    clrlslwi r9, r9, 17, 1
+    addi r10, r10, 1
+
+_make_huffdec_values_loop:
+    lwz r0, 0(r7)
+    cmpwi r0, 0
+    bne _make_huffdec_values_inner
+    mr r3, r30
+    li r4, 0
+    li r5, 0x44
+    bl memset
+    addi r4, r1, 0x408
+    addi r5, r1, 8
+    li r6, 0
+    mtctr r28
+    cmpwi r28, 0
+    ble _make_huffdec_valptr_done
+
+_make_huffdec_valptr_loop:
+    lwz r0, 0(r4)
+    cmplwi r0, 0x10
+    bgt _make_huffdec_valptr_next
+    slwi r3, r0, 2
+    lwz r0, 0(r5)
+    add r3, r30, r3
+    sth r6, 2(r3)
+    sth r0, 0(r3)
+
+_make_huffdec_valptr_next:
+    addi r4, r4, 4
+    addi r5, r5, 4
+    addi r6, r6, 1
+    bdnz _make_huffdec_valptr_loop
+
+_make_huffdec_valptr_done:
+    addi r4, r1, 8
+    addi r12, r26, 1
+    li r6, 0
+    li r7, 1
+    lis r3, 0x8000
+
+_make_huffdec_table_loop:
+    subfic r0, r7, 8
+    li r8, 1
+    slw r5, r8, r0
+    b _make_huffdec_table_count
+
+_make_huffdec_table_store:
+    lwz r10, 0(r4)
+    cmpwi cr1, r5, 0
+    mr r9, r5
+    slw r10, r10, r0
+    ble cr1, _make_huffdec_table_next
+    cmpwi r5, 8
+    ble _make_huffdec_table_short
+    li r21, 0
+    blt cr1, _make_huffdec_table_short_check
+    addi r11, r3, 1
+    cmpw r5, r11
+    blt _make_huffdec_table_short_check
+    li r21, 1
+
+_make_huffdec_table_short_check:
+    cmpwi r21, 0
+    beq _make_huffdec_table_short
+    subi r11, r5, 1
+    slwi r26, r10, 2
+    srwi r11, r11, 3
+    mtctr r11
+    cmpwi r5, 8
+    ble _make_huffdec_table_short
+
+_make_huffdec_table_unrolled:
+    sthx r7, r31, r26
+    add r25, r31, r26
+    addi r11, r10, 1
+    addi r22, r10, 2
+    lbzx r24, r27, r6
+    slwi r21, r11, 2
+    addi r11, r10, 3
+    addi r30, r10, 6
+    sth r24, 2(r25)
+    slwi r23, r11, 2
+    addi r11, r10, 4
+    add r20, r31, r21
+    sthx r7, r31, r21
+    slwi r24, r11, 2
+    addi r11, r10, 5
+    slwi r22, r22, 2
+    lbzx r21, r27, r6
+    slwi r25, r11, 2
+    addi r11, r10, 7
+    slwi r30, r30, 2
+    sth r21, 2(r20)
+    add r20, r31, r23
+    add r21, r31, r24
+    slwi r11, r11, 2
+    sthux r7, r22, r31
+    add r23, r31, r25
+    add r25, r31, r11
+    add r24, r31, r30
+    lbzx r11, r27, r6
+    addi r10, r10, 8
+    addi r26, r26, 0x20
+    subi r9, r9, 8
+    sth r11, 2(r22)
+    sth r7, 0(r20)
+    lbzx r11, r27, r6
+    sth r11, 2(r20)
+    sth r7, 0(r21)
+    lbzx r11, r27, r6
+    sth r11, 2(r21)
+    sth r7, 0(r23)
+    lbzx r11, r27, r6
+    sth r11, 2(r23)
+    sthx r7, r31, r30
+    lbzx r11, r27, r6
+    sth r11, 2(r24)
+    sth r7, 0(r25)
+    lbzx r11, r27, r6
+    sth r11, 2(r25)
+    bdnz _make_huffdec_table_unrolled
+
+_make_huffdec_table_short:
+    slwi r10, r10, 2
+    add r11, r31, r10
+    mtctr r9
+    cmpwi r9, 0
+    ble _make_huffdec_table_next
+
+_make_huffdec_table_short_loop:
+    sth r7, 0(r11)
+    subi r9, r9, 1
+    lbzx r10, r27, r6
+    sth r10, 2(r11)
+    addi r11, r11, 4
+    bdnz _make_huffdec_table_short_loop
+
+_make_huffdec_table_next:
+    addi r8, r8, 1
+    addi r4, r4, 4
+    addi r6, r6, 1
+
+_make_huffdec_table_count:
+    lbz r9, 0(r12)
+    cmpw r8, r9
+    ble _make_huffdec_table_store
+    addi r7, r7, 1
+    addi r12, r12, 1
+    cmpwi r7, 8
+    ble _make_huffdec_table_loop
+    mr r3, r29
+    mr r4, r27
+    mr r5, r28
+    bl memcpy
+    li r3, 0
+
+_make_huffdec_return:
+    lmw r20, 0x810(r1)
+    lwz r0, 0x844(r1)
+    mtlr r0
+    addi r1, r1, 0x840
+    blr
+}
+#else
 s32 TMCJPEGDEC_make_huffdec(const u8* dht, const u8* tb, TMCHuffParam* hp) {
     u32 huffCount[256];
     u32 huffVal[256];
@@ -115,6 +366,7 @@ s32 TMCJPEGDEC_make_huffdec(const u8* dht, const u8* tb, TMCHuffParam* hp) {
 
     return 0;
 }
+#endif
 
 void TMCJPEGDEC_set_HuffmanTable(TMCHuffParam* tbl, s32 tblType, s32 tblID, TMCUnknownInfo* work) {
     void* hufftable;
