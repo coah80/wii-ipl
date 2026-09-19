1feaba07f34edc3ff70eb0ad94a7b36844677819
coah <100182500+coah80@users.noreply.github.com>
2026-09-19 18:24:32 +0000

    Match EGG ArcPlayer startSound

--- libs/EGG/include/egg/core/eggAudioArcPlayerMgr.h
+++ libs/EGG/include/egg/core/eggAudioArcPlayerMgr.h
@@ -40,12 +40,8 @@ namespace EGG {
 
         virtual bool startSound(nw4r::snd::SoundHandle* pHandle, u32 id) { return mSoundArchivePlayer.StartSound(pHandle, id); }           // 0x34
         virtual bool startSound(nw4r::snd::SoundHandle* pHandle, unsigned int id) { return mSoundArchivePlayer.StartSound(pHandle, id); }  // 0x38
-        virtual bool startSound(nw4r::snd::SoundHandle* pHandle, const char* pName) {
-            u32 id = nw4r::snd::SoundArchive::INVALID_ID;
-            if (mpArchive != NULL) {
-                id = changeNameToId(pName);
-            }
-
+        virtual bool startSound(nw4r::snd::SoundHandle* pHandle, const char* pName) NO_INLINE {
+            u32 id = changeNameToId(pName);
             return mSoundArchivePlayer.StartSound(pHandle, id);
         }  // 0x3C
 
