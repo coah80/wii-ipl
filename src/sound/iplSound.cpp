babc9ba9cdf4561caa2778de3809084a0292529b
coah <100182500+coah80@users.noreply.github.com>
2026-09-19 17:48:13 +0000

    Match snd::System::shutup

--- src/sound/iplSound.cpp
+++ src/sound/iplSound.cpp
@@ -1,9 +1,23 @@
 #include "iplSound.h"
 #include "sound/iplSound.h"
 
+#include <revolution/ai.h>
+
 namespace ipl {
     namespace snd {
+        BOOL m_isLocked;
+
         void System::shutup(BOOL shutUpDMA) {
+            if (m_isLocked == shutUpDMA) {
+                return;
+            }
+
+            m_isLocked = shutUpDMA;
+            if (shutUpDMA) {
+                AIStopDMA();
+            } else {
+                AIStartDMA();
+            }
         }
     }  // namespace snd
 }  // namespace ipl
