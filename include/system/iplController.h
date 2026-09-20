81ed31027c8344818c15b672c68b14a53632e0f9
coah <100182500+coah80@users.noreply.github.com>
2026-09-20 13:36:14 +0000

    Match controller horizon

--- include/system/iplController.h
+++ include/system/iplController.h
@@ -168,6 +168,7 @@ namespace ipl {
             virtual bool isValidDpd() const override;
             virtual math::VEC2 getDpdPos() const override;
             virtual math::VEC2 getDpdProjectionPos() const override;
+            virtual math::VEC2 getHorizon() const override;
             virtual void read() override;
         };
 
