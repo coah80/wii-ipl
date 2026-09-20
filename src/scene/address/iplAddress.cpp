0e8b09b0b93741954c2f05e2caa8bc5787d45453
coah <100182500+coah80@users.noreply.github.com>
2026-09-20 13:48:59 +0000

    Match friend cache finalizer

--- src/scene/address/iplAddress.cpp
+++ src/scene/address/iplAddress.cpp
@@ -3,6 +3,7 @@
 extern "C" char smArg__Q23ipl6System;
 extern "C" void checkUserId__Q33ipl5nwc247ManagerFUx();
 extern "C" void getErrCode__Q33ipl5nwc247ManagerFv();
+extern "C" void close__Q33ipl5nwc247ManagerFv();
 
 extern "C" asm void getChild__Q33ipl5scene4BaseFv() {
     nofralloc
@@ -91,4 +92,24 @@ getErrCode_L1:
     lwz r3, 0x8c(r3)
 getErrCode_L2:
     b getErrCode__Q33ipl5nwc247ManagerFv
+}
+
+extern "C" asm void fin__Q33ipl5scene15FriendListCacheFv() {
+    nofralloc
+    addis r3, r3, 1
+    lbz r0, -0x6288(r3)
+    cmpwi r0, 0
+    beqlr
+    lis r3, smArg__Q23ipl6System@ha
+    addi r3, r3, smArg__Q23ipl6System@l
+    lbz r0, 0x2bc(r3)
+    cmpwi r0, 0
+    beq fin_L1
+    li r3, 0
+    b fin_L2
+fin_L1:
+    lwz r3, 0x8c(r3)
+fin_L2:
+    b close__Q33ipl5nwc247ManagerFv
+    blr
 }
