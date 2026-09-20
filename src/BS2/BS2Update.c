73a284a26327f71dd9817b57d7a08e044c4d46b1
coah <100182500+coah80@users.noreply.github.com>
2026-09-20 13:11:08 +0000

    Match BS2 update accessors

---
+++ src/BS2/BS2Update.c
@@ -0,0 +1,49 @@
+#include "BS2/BS2Update.h"
+
+static BS2UpdateEntry* pEntries;
+static u32 EntriesCount;
+static u32* pFlags;
+static void* MemAllocator;
+int State;
+BS2UpdateEntry* CurrentEntry;
+static u32 lbl_81698B10;
+static BOOL lbl_81698B14;
+static u32 lbl_81698B18;
+static u32 lbl_81698B1C;
+u32 StartUpdate;
+u32 CancelUpdate;
+static f32 UpdateProgress;
+static s32 rc;
+static u64 VersionIOS;
+static u32 VersionMEM2;
+static u32 VersionES;
+static u32 ConsoleType;
+static void* FatalFunc;
+
+int BS2UpdateState() {
+    return State;
+}
+
+void BS2StartUpdate() {
+    StartUpdate = 1;
+}
+
+void BS2CancelUpdate() {
+    CancelUpdate = 1;
+}
+
+BS2UpdateEntry* BS2GetUpdateEntry() {
+    return pEntries;
+}
+
+u32 BS2GetUpdateEntryNum() {
+    return EntriesCount;
+}
+
+BS2UpdateEntry* BS2GetCurrentEntry() {
+    return CurrentEntry;
+}
+
+BOOL BS2ContainsSeatTitles() {
+    return lbl_81698B14;
+}
