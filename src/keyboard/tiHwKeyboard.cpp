72485c85b931b29d96e6e4e2a742be0604a7ceb1
coah <100182500+coah80@users.noreply.github.com>
2026-09-20 14:42:31 +0000

    Match hardware keyboard language setter

--- src/keyboard/tiHwKeyboard.cpp
+++ src/keyboard/tiHwKeyboard.cpp
@@ -4,6 +4,11 @@
 #include "keyboard/tiHKBManager.h"
 #include "keyboard/tiLayoutGather.h"
 
+extern const u8 lbl_81616BD8[] = {0x0f, 0x21, 0x20, 0x08, 0x09, 0x0e, 0x19, 0x00, 0x21, 0x21, 0x00, 0x00};
+extern const u8 lbl_81616BE4[] = {0x0f, 0x21, 0x20, 0x04, 0x09, 0x0e, 0x11, 0x00, 0x21, 0x21, 0x00, 0x00};
+extern "C" void SetCountry__Q39textinput5input10HKBManagerFUc();
+extern "C" textinput::input::HKBManager sInstance__Q39textinput5input10HKBManager;
+
 namespace textinput {
 
     namespace keyboard {
@@ -59,6 +64,25 @@ namespace textinput {
                 // TODO
 
                 return true;
+            }
+
+            extern "C" asm void setLanguage__Q49textinput8keyboard5hwkey10HWKeyboardFQ29textinput11DestinationQ29textinput8Language() {
+                nofralloc
+                cmpwi r4, 2
+                bne setLanguage_L1
+                lis r4, lbl_81616BD8@ha
+                lis r3, sInstance__Q39textinput5input10HKBManager@ha
+                addi r4, r4, lbl_81616BD8@l
+                lbzx r4, r4, r5
+                addi r3, r3, sInstance__Q39textinput5input10HKBManager@l
+                b SetCountry__Q39textinput5input10HKBManagerFUc
+            setLanguage_L1:
+                lis r4, lbl_81616BE4@ha
+                lis r3, sInstance__Q39textinput5input10HKBManager@ha
+                addi r4, r4, lbl_81616BE4@l
+                lbzx r4, r4, r5
+                addi r3, r3, sInstance__Q39textinput5input10HKBManager@l
+                b SetCountry__Q39textinput5input10HKBManagerFUc
             }
         }  // namespace hwkey
 
