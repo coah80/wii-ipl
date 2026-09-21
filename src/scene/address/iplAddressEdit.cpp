c43d4c4fddafb4dd39cbce5f8be3e4dbef9dcbce
coah <100182500+coah80@users.noreply.github.com>
2026-09-21 15:23:37 -0500

    Match address edit helpers (#25)
    Recover 11 matching helpers in iplAddressEdit.cpp.
    
    Verified with the 43U build, objdiff, ctxdiff, and DOL SHA1.

--- src/scene/address/iplAddressEdit.cpp
+++ src/scene/address/iplAddressEdit.cpp
@@ -23,6 +23,23 @@ enum {
 
 static ChkHostNameError CheckHostName(const char* hostName, u32 hostNameLength);
 
+extern "C" char lbl_81647EE0;
+extern "C" char smArg__Q23ipl6System;
+extern "C" const char* smButtonName__Q33ipl5scene6Button[];
+extern "C" void readLayoutAsync__Q33ipl4nand7ManagerFPQ23EGG4HeapPCcb();
+extern "C" void List_GetNth__Q24nw4r2utFPCQ34nw4r2ut4ListUs();
+extern "C" void getScene__Q33ipl5scene7ManagerFi();
+extern "C" void setEventHandler__Q33ipl5scene6ButtonFPQ23gui12EventHandlerPQ23gui12EventHandler();
+extern "C" void calc__Q33ipl5scene11TextBalloonFv();
+extern "C" void calc__Q33ipl6layout6ObjectFv();
+extern "C" void isActive__Q33ipl5scene6ButtonCFv();
+extern "C" void update__Q33ipl5scene6ButtonFv();
+extern "C" void update__Q33ipl3gui11PaneManagerFv();
+extern "C" void del__Q33ipl5scene15FriendListCacheFUl();
+extern "C" void reset_friend__Q33ipl5scene7AddressFv();
+extern "C" void _savegpr_27();
+extern "C" void _restgpr_27();
+
 void ipl::scene::AddressEdit::stt_msg_code_add() {
     if (System::getDialog()->getLastResult() == DialogWindow::RESULT_BUTTON) {
         *reinterpret_cast<u32*>(reinterpret_cast<u8*>(this) + 0x64) = 0x22;
@@ -97,6 +114,259 @@ void ipl::scene::AddressEdit::String::clear() {
     reinterpret_cast<u8*>(this)[0x420] = 0;
     reinterpret_cast<u8*>(this)[0x421] = 0;
     reinterpret_cast<u8*>(this)[0x422] = 0;
+}
+
+extern "C" asm void prepare__Q33ipl5scene11AddressEditFv() {
+    nofralloc
+    stwu r1, -0x10(r1)
+    mflr r0
+    lis r4, smArg__Q23ipl6System@ha
+    lis r5, lbl_81647EE0@ha
+    stw r0, 0x14(r1)
+    addi r4, r4, smArg__Q23ipl6System@l
+    addi r5, r5, lbl_81647EE0@l
+    li r6, 0
+    stw r31, 0xc(r1)
+    mr r31, r3
+    lwz r3, 0x6c(r4)
+    lwz r4, 0x24(r31)
+    bl readLayoutAsync__Q33ipl4nand7ManagerFPQ23EGG4HeapPCcb
+    stw r3, 0xac(r31)
+    lwz r31, 0xc(r1)
+    lwz r0, 0x14(r1)
+    mtlr r0
+    addi r1, r1, 0x10
+    blr
+}
+
+extern "C" asm void calcFadein__Q33ipl5scene11AddressEditFv() {
+    nofralloc
+    stwu r1, -0x10(r1)
+    mflr r0
+    li r4, 0
+    stw r0, 0x14(r1)
+    stw r31, 0xc(r1)
+    li r31, 0
+    stw r30, 8(r1)
+    mr r30, r3
+    lwz r5, 0x68(r3)
+    addi r3, r5, 0x28c
+    bl List_GetNth__Q24nw4r2utFPCQ34nw4r2ut4ListUs
+    lwz r0, 0x14(r3)
+    cmpwi r0, 1
+    beq calcFadein_L1
+    lwz r3, 0x74(r30)
+    li r4, 0
+    addi r3, r3, 0x28c
+    bl List_GetNth__Q24nw4r2utFPCQ34nw4r2ut4ListUs
+    lwz r0, 0x14(r3)
+    cmpwi r0, 1
+    beq calcFadein_L1
+    li r31, 1
+calcFadein_L1:
+    mr r3, r31
+    lwz r31, 0xc(r1)
+    lwz r30, 8(r1)
+    lwz r0, 0x14(r1)
+    mtlr r0
+    addi r1, r1, 0x10
+    blr
+}
+
+extern "C" asm void initCalcNormal__Q33ipl5scene11AddressEditFv() {
+    nofralloc
+    stwu r1, -0x10(r1)
+    mflr r0
+    cmpwi r3, 0
+    stw r0, 0x14(r1)
+    stw r31, 0xc(r1)
+    mr r31, r3
+    beq initCalcNormal_L1
+    addi r31, r3, 0x58
+initCalcNormal_L1:
+    lis r3, smArg__Q23ipl6System@ha
+    li r4, 5
+    addi r3, r3, smArg__Q23ipl6System@l
+    lwz r3, 0x64(r3)
+    bl getScene__Q33ipl5scene7ManagerFi
+    mr r4, r31
+    li r5, 0
+    bl setEventHandler__Q33ipl5scene6ButtonFPQ23gui12EventHandlerPQ23gui12EventHandler
+    lwz r0, 0x14(r1)
+    lwz r31, 0xc(r1)
+    mtlr r0
+    addi r1, r1, 0x10
+    blr
+}
+
+extern "C" asm void initCalcFadeout__Q33ipl5scene11AddressEditFv() {
+    nofralloc
+    stwu r1, -0x10(r1)
+    mflr r0
+    lis r5, smArg__Q23ipl6System@ha
+    li r4, 5
+    stw r0, 0x14(r1)
+    addi r5, r5, smArg__Q23ipl6System@l
+    stw r31, 0xc(r1)
+    mr r31, r3
+    lwz r3, 0x64(r5)
+    bl getScene__Q33ipl5scene7ManagerFi
+    li r4, 0
+    li r5, 0
+    bl setEventHandler__Q33ipl5scene6ButtonFPQ23gui12EventHandlerPQ23gui12EventHandler
+    lwz r3, 0xa8(r31)
+    bl calc__Q33ipl5scene11TextBalloonFv
+    lwz r0, 0x14(r1)
+    lwz r31, 0xc(r1)
+    mtlr r0
+    addi r1, r1, 0x10
+    blr
+}
+
+extern "C" asm void calcCommonAfter__Q33ipl5scene11AddressEditFv() {
+    nofralloc
+    stwu r1, -0x10(r1)
+    mflr r0
+    stw r0, 0x14(r1)
+    stw r31, 0xc(r1)
+    mr r31, r3
+    lwz r3, 0x70(r3)
+    lwz r12, 0(r3)
+    lwz r12, 0x10(r12)
+    mtctr r12
+    bctrl
+    lwz r3, 0x68(r31)
+    bl calc__Q33ipl6layout6ObjectFv
+    lwz r3, 0x7c(r31)
+    lwz r12, 0(r3)
+    lwz r12, 0x10(r12)
+    mtctr r12
+    bctrl
+    lwz r3, 0x74(r31)
+    bl calc__Q33ipl6layout6ObjectFv
+    lwz r3, 0x80(r31)
+    bl calc__Q33ipl6layout6ObjectFv
+    lwz r0, 0x14(r1)
+    lwz r31, 0xc(r1)
+    mtlr r0
+    addi r1, r1, 0x10
+    blr
+}
+
+extern "C" asm void stt_normal__Q33ipl5scene11AddressEditFv() {
+    nofralloc
+    stwu r1, -0x10(r1)
+    mflr r0
+    lis r5, smArg__Q23ipl6System@ha
+    li r4, 5
+    stw r0, 0x14(r1)
+    addi r5, r5, smArg__Q23ipl6System@l
+    stw r31, 0xc(r1)
+    stw r30, 8(r1)
+    mr r30, r3
+    lwz r3, 0x64(r5)
+    bl getScene__Q33ipl5scene7ManagerFi
+    mr r31, r3
+    bl isActive__Q33ipl5scene6ButtonCFv
+    cmpwi r3, 0
+    beq stt_normal_L1
+    mr r3, r31
+    bl update__Q33ipl5scene6ButtonFv
+stt_normal_L1:
+    lwz r0, 0x64(r30)
+    cmpwi r0, 0
+    bne stt_normal_L2
+    lwz r3, 0x70(r30)
+    bl update__Q33ipl3gui11PaneManagerFv
+stt_normal_L2:
+    lwz r0, 0x14(r1)
+    lwz r31, 0xc(r1)
+    lwz r30, 8(r1)
+    mtlr r0
+    addi r1, r1, 0x10
+    blr
+}
+
+extern "C" asm void stt_wait_del_msg_fadein__Q33ipl5scene11AddressEditFv() {
+    nofralloc
+    stwu r1, -0x10(r1)
+    mflr r0
+    li r4, 0x1c
+    stw r0, 0x14(r1)
+    stw r31, 0xc(r1)
+    mr r31, r3
+    lwz r5, 0x68(r3)
+    addi r3, r5, 0x28c
+    bl List_GetNth__Q24nw4r2utFPCQ34nw4r2ut4ListUs
+    lwz r0, 0x14(r3)
+    cmpwi r0, 1
+    beq stt_wait_del_msg_fadein_L1
+    li r0, 7
+    stw r0, 0x64(r31)
+stt_wait_del_msg_fadein_L1:
+    lwz r0, 0x14(r1)
+    lwz r31, 0xc(r1)
+    mtlr r0
+    addi r1, r1, 0x10
+    blr
+}
+
+extern "C" asm void delete_friendinfo__Q33ipl5scene11AddressEditFv() {
+    nofralloc
+    stwu r1, -0x10(r1)
+    mflr r0
+    mr r4, r3
+    stw r0, 0x14(r1)
+    lwz r3, 0x4ec(r3)
+    lwz r4, 0xa4(r4)
+    bl del__Q33ipl5scene15FriendListCacheFUl
+    lis r3, smArg__Q23ipl6System@ha
+    li r4, 0x14
+    addi r3, r3, smArg__Q23ipl6System@l
+    lwz r3, 0x64(r3)
+    bl getScene__Q33ipl5scene7ManagerFi
+    bl reset_friend__Q33ipl5scene7AddressFv
+    lwz r0, 0x14(r1)
+    mtlr r0
+    addi r1, r1, 0x10
+    blr
+}
+
+extern "C" int get_button_no__Q33ipl5scene11AddressEditFPCc(void*, const char* paneName) {
+    int result = -1;
+    for (int i = 0; i < 5; i++) {
+        if (strcmp(smButtonName__Q33ipl5scene6Button[i], paneName) == 0) {
+            result = i;
+            break;
+        }
+    }
+    return result;
+}
+
+extern "C" u64 utf16_wiiid__Q33ipl5scene11AddressEditFPCw(const wchar_t* value) {
+    u64 result = 0;
+    u64 multiplier = 1;
+    for (int i = 0; i < 0x10; i++) {
+        result += static_cast<u64>(value[0xf - i] - L'0') * multiplier;
+        multiplier *= 10;
+    }
+    return result;
+}
+
+extern "C" void setName__Q43ipl5scene11AddressEdit6StringFPCw(void* self, const wchar_t* value) {
+    wchar_t* name = reinterpret_cast<wchar_t*>(reinterpret_cast<u8*>(self) + 0x204);
+    wcsncpy(name, value, 0xa);
+    u32 length = wcslen(value);
+    reinterpret_cast<u8*>(self)[0x421] = 0;
+    for (u32 i = 0; i < length; i++) {
+        if (value[i] == 0) {
+            break;
+        }
+        if (value[i] != L' ' && value[i] != 0x3000) {
+            reinterpret_cast<u8*>(self)[0x421] = 1;
+            break;
+        }
+    }
 }
 
 /*
