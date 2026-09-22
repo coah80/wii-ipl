47506ee4d569fc805d8f049f1047e32d37d1d386
coah <100182500+coah80@users.noreply.github.com>
2026-09-22 07:48:22 +0000

    Match MSL direct IO

--- configure.py
+++ configure.py
@@ -1949,7 +1949,7 @@ config.libs = [
             Object(Matching,    "MSL_Common/arith.c"),
             Object(Matching,    "MSL_Common/bsearch.c"),
             Object(Matching,    "MSL_Common/buffer_io.c"),
-            Object(NonMatching, "MSL_Common/direct_io.c"),
+            Object(Matching,    "MSL_Common/direct_io.c", extra_cflags=["-Cpp_exceptions on"]),
             Object(NonMatching, "MSL_Common/file_io.c"),
             Object(NonMatching, "MSL_Common/FILE_POS.c"),
             Object(Matching,    "MSL_Common/mbstring.c"),
