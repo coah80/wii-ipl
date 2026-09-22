584dbb69062985814676206e16a53d2c36dff782
coah <100182500+coah80@users.noreply.github.com>
2026-09-22 02:51:42 -0500

    Match MSL multibyte string helpers (#152)

--- configure.py
+++ configure.py
@@ -1952,7 +1952,7 @@ config.libs = [
             Object(NonMatching, "MSL_Common/direct_io.c"),
             Object(NonMatching, "MSL_Common/file_io.c"),
             Object(NonMatching, "MSL_Common/FILE_POS.c"),
-            Object(NonMatching, "MSL_Common/mbstring.c"),
+            Object(Matching,    "MSL_Common/mbstring.c"),
             Object(Matching,    "MSL_Common/mem.c"),
             Object(Matching,    "MSL_Common/mem_funcs.c"),
             Object(Matching,    "MSL_Common/math_api.c"),
