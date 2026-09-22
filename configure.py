370e04648e6bfd05445405576530133fe604e3e7
coah <100182500+coah80@users.noreply.github.com>
2026-09-22 08:30:22 +0000

    Link matching www browser unit

--- configure.py
+++ configure.py
@@ -692,7 +692,7 @@ config.libs = [
         ]
     ),
     IPLSection("iplwww", [
-            Object(Equivalent,  "iplwww/www_browser.cpp"),
+            Object(Matching,    "iplwww/www_browser.cpp"),
             Object(Matching,    "iplwww/www_message.cpp"),
             Object(Matching,    "iplwww/www_surface.cpp"),
             Object(Matching,    "iplwww/www_thread.cpp"),
