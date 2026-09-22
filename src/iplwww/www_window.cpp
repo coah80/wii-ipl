0a6a722237021ef74d89716dab5f8bc6c80b7179
coah <100182500+coah80@users.noreply.github.com>
2026-09-21 23:23:06 -0500

    Match and link www_window (#93)
    * Match BrowserWindow tile blit
    
    * Match BrowserWindow tile row setup
    
    * Link matching BrowserWindow unit

--- src/iplwww/www_window.cpp
+++ src/iplwww/www_window.cpp
@@ -211,7 +211,6 @@ namespace ext_ead {
                 return;
             }
 
-            u32 srcBlockAdvance;
             u32 srcRowAdvance;
             void* rasterSrc;
             u16* blockRowDst;
@@ -221,13 +220,13 @@ namespace ext_ead {
             rasterSrc = mpBrowserThread->GetRaster();
             blockRowDst = (u16*)GetTextureBuffer(0, TRUE, NULL);
 
-            srcBlockAdvance = srcRowAdvance * sizeof(u32);
             for (int blockY = 0; blockY < mHeight; blockY += 4) {
                 u16* blockDst = blockRowDst;
                 SubRow* blockRowA = (SubRow*)rasterSrc;
                 SubRow* blockRowB = (SubRow*)((u8*)rasterSrc + srcRowAdvance);
                 SubRow* blockRowC = (SubRow*)((u8*)blockRowB + srcRowAdvance);
-                SubRow* blockRowD = (SubRow*)((u8*)blockRowC + srcRowAdvance);
+                u8* blockRowDBytes = (u8*)blockRowB + srcRowAdvance;
+                SubRow* blockRowD = (SubRow*)(blockRowDBytes + srcRowAdvance);
 
                 for (int blockX = 0; blockX < mWidth; blockX += 4) {
                     blockDst[0] = convertToRGB565((*blockRowA)[0]);
@@ -252,7 +251,7 @@ namespace ext_ead {
 
                     blockDst += 16;
                 }
-                rasterSrc = (u8*)rasterSrc + srcBlockAdvance;
+                rasterSrc = (u8*)rasterSrc + srcRowAdvance * sizeof(u32);
                 blockRowDst += mWidth * 4;
             }
 
