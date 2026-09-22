a8c6bcf550c17d9b885ff382334a6e2d96f31cda
coah <100182500+coah80@users.noreply.github.com>
2026-09-22 05:15:58 +0000

    Match Zi8GetZHuwdPtr

--- libs/RVLMiddleware/eZiText/src/clib/zi8ZHuwd.c
+++ libs/RVLMiddleware/eZiText/src/clib/zi8ZHuwd.c
@@ -9,7 +9,7 @@ ziU32 Zi8GetZHuwdPtr(ziU32* arg0, ziU16* arg1 ZI_NEED_WORK) {
         return 0;
     } else {
         *arg1 = ZI_WORK->unk_0x124[ZI_WORK->unk_0x12C - 1][1] / 3;
-        *arg0 = ZI_WORK->unk_0x124[ZI_WORK->unk_0x12C - 1][0];
+        *arg0 = (ziU32)ZI_WORK->unk_0x124[ZI_WORK->unk_0x12C - 1];
         *arg0 += 8;
         Zi8LogError(0x64, ZI_WORK);
         return 1;
